af301d337484a2e9babdf47a4238b783cbe1e2f3
zolsen10 <soccerzack10@yahoo.com>
2022-09-26 14:04:16 -0600

    Added in my name and organized the list better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Thayne Evans
-
 # Karl Nordgren added this new line of code. It is a comment. Nice job team captian. I hope this works. Are you still reading this? 
 # Oliver Escobar
 # Tayler Howard
-#Ashley Ondoua
+# Ashley Ondoua
+# Zack Olsen
